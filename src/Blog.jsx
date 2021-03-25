8c75b5c03a88dd1cf79ef43a4db59277fe5dec02
MoekoTakahashi <moeko.takahashi@amptech.co.jp>
2021-03-25 16:20:51 +0900

    React入門#8

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,50 +1,44 @@
 import React from 'react';
 import Article from "./Article";
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
 
-  componentDidMount() {
-    document.getElementById('counter').addEventListener('click',this.countUp)
-  }
-  componentDidUpdate() {
-
-    if (this.state.count >= 10){
-      this.setState({count: 0})
-    }
-  }
+const Blog = () => {
+    
   
-  componentWillUnmount () {
-    document.getElementById('counter').removeEventListener('click',this.countUp)
-  }
 
-  togglePublished = () => {
-    this.setState ({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // componentDidMount() {
+  //   document.getElementById('counter').addEventListener('click',this.countUp)
+  // }
+  // componentDidUpdate() {
 
-  countUp = () => {
-    this.setState({count: this.state.count + 1
-    });
-  }
-  render(){
-    const authorName = "Torahack"
-    return (
+  //   if (this.state.count >= 10){
+  //     this.setState({count: 0})
+  //   }
+  // }
+  
+  // componentWillUnmount () {
+  //   document.getElementById('counter').removeEventListener('click',this.countUp)
+  // }
+
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1
+  //   });
+  // }
+  
+  return (
       <>
-       <Article title={"Reactの使い方"} 
-       isPublished={this.state.isPublished} 
-       toggle={()=>this.togglePublished()}
-       count={this.state.count}/>  
+     <Article 
+     title={"Reactの使い方"} 
+    //  count={this.state.count}
+     /> 
+
+     <FooBar.Foo/>
+     <FooBar.Bar/>
+     <Hoge/>     
            
-      </>
-    )
-  }
+    </>
+  )
 }
 
 export default Blog
---
+++ src/components/FooBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Foo() {
+  return(<h2>FOoooo</h2>)
+}
+
+export const Bar = () => {
+  return(<h2>Baaaaa</h2>)};
